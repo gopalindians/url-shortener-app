d94bef7ae45f7be149c33f419791e4410354396e
Gopal Sharma <gopalindians@gmail.com>
2017-01-13 20:37:23 +0530

    :heart_eyes: addded form for  inputting long url

---
+++ application/views/url-app/url/index.php
@@ -0,0 +1,19 @@
+<?php if ('' !== validation_errors()): ?>
+    <div class="alert alert-danger" role="alert"><?= validation_errors(); ?></div>
+<?php endif; ?>
+
+
+<?php if (NULL !== $this->session->flashdata('url_success')): ?>
+<div class="alert alert-success" role="alert">
+    Shortened url is <a href="<?= base_url('url-app/url/r/') . $this->session->flashdata('url_success'); ?>"><?= base_url('url-app/url/r/') . $this->session->flashdata('url_success'); ?></a>
+    </div>
+<?php endif; ?>
+
+<div class=" col-lg-offset-4">
+    <?= form_open('/url-app/url/handleUrl', ['class' => 'form-inline']) ?>
+    <div class="form-group">
+        <input type="url" class="form-control" id="url" placeholder="http://mylongurl.com" name="url">
+    </div>
+    <button type="submit" class="btn btn-default">Shorten it</button>
+    <?= form_close() ?>
+</div>
