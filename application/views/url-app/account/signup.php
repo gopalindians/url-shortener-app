de50b1ea55fce264254cd94ab9cfa562e21b07b5
Gopal Sharma <gopalindians@gmail.com>
2017-01-19 23:45:31 +0530

    Added Private google reCAPTCHA key

--- application/views/url-app/account/signup.php
+++ application/views/url-app/account/signup.php
@@ -5,25 +5,27 @@
     <div class="form-group">
         <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
         <div class="col-sm-10">
-            <input type="email" class="form-control"  placeholder="Email" name="email" value="<?= set_value('email'); ?>" required>
+            <input type="email" class="form-control" placeholder="Email" name="email" value="<?= set_value('email'); ?>"
+                   required>
         </div>
     </div>
     <div class="form-group">
         <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control"  placeholder="Password" name="password" required>
+            <input type="password" class="form-control" placeholder="Password" name="password" required>
         </div>
     </div>
 
     <div class="form-group">
         <label for="inputPassword3" class="col-sm-2 control-label">Password Confirm</label>
         <div class="col-sm-10">
-            <input type="password" class="form-control"  placeholder="Password Confirm"
+            <input type="password" class="form-control" placeholder="Password Confirm"
                    name="passwordConfirm" required>
         </div>
     </div>
 
-    <div class="g-recaptcha pull-right" data-sitekey="ADD_YOUR_OWN_KEY_HERE"></div>
+    <!--Add your own key here-->
+    <div class="g-recaptcha pull-right" data-sitekey="6LedKxEUAAAAADdyZQl6bRUI1gQYgWzuij7L9ELm"></div>
     <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
             <button type="submit" class="btn btn-success">Sign up</button>
