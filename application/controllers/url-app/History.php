4c25b803c568b3f679f38d145365f472c16fce1a
Gopal Sharma <gopalindians@gmail.com>
2017-01-19 23:20:02 +0530

    Added History controller

--- application/controllers/url-app/History.php
+++ application/controllers/url-app/History.php
@@ -6,7 +6,51 @@
  * Date: 19-01-2017
  * Time: 17:00
  */
-class Histroy
+class History extends CI_Controller
 {
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        if ($this->session->userdata('email') === NULL) {
+            redirect('url-app/account/login');
+        }
+
+    }
+
+    public function index()
+    {
+
+        $userEmail = $this->session->userdata('email');
+        $userData = $this->Account_Model->getUserData($userEmail);
+        $userId = $userData[0]['id'];
+        $urlHistory = $this->History_Model->getUrlHistory($userId);
+
+
+        if ($urlHistory === false) {
+            $urlHistory = [];
+        }
+
+        $this->load->view('url-app/include/header.php');
+        $this->load->view('url-app/history/index.php', ['urlHistory' => $urlHistory]);
+        $this->load->view('url-app/include/footer.php');
+
+    }
+
+    public function getMore()
+    {
+
+
+        $offset = $this->input->get('offset');
+        $userEmail = $this->session->userdata('email');
+        $userData = $this->Account_Model->getUserData($userEmail);
+        $userId = $userData[0]['id'];
+
+        $urlHistory = $this->History_Model->getUrlHistory($userId, 5, $offset);
+        header('Content-Type:application/json');
+        echo json_encode($urlHistory);
+
+    }
 
 }
