9d654f4f05ffde604aecefc3e9e8e29f1ab73bc8
Gopal Sharma <gopalindians@gmail.com>
2017-01-19 23:47:37 +0530

    move jquery script file from footer to header

--- application/views/url-app/include/footer.php
+++ application/views/url-app/include/footer.php
@@ -1,6 +1,5 @@
 </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
+
 
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
