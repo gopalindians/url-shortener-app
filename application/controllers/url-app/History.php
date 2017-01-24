b84aaa803a7a441b3cba525a37b7dcce9f6045bc
Gopal Sharma <gopalindians@gmail.com>
2017-01-24 16:38:37 +0530

    Added safety measure to filter out non ajax request

--- application/controllers/url-app/History.php
+++ application/controllers/url-app/History.php
@@ -40,9 +40,23 @@ class History extends CI_Controller
 
     public function getMore()
     {
+        if (!$this->input->is_ajax_request()) {
+            $result = ['error' => 'Not allowed'];
+            header('Content-Type:application/json');
+            echo json_encode($result);
+            return;
+        }
 
 
-        $offset = $this->input->get('offset');
+        if (!is_int(abs($this->input->get('offset')))) {
+            $result = ['error' => 'Not allowed'];
+            header('Content-Type:application/json');
+            echo json_encode($result);
+            return;
+        }
+
+
+        $offset = abs(trim($this->input->get('offset')));
         $userEmail = $this->session->userdata('email');
         $userData = $this->Account_Model->getUserData($userEmail);
         $userId = $userData[0]['id'];
